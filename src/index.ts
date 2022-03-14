104b29c572133abad6471872af2b0d6a8dd261c5
danimal141 <hideaki.ishii1204@gmail.com>
2022-03-14 22:38:56 +0900

    Use refresh token instead because access token expires soon

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { Dropbox } from "dropbox";
 import DbxAdapter from "./dropbox/adapter";
 
 (async () => {
-  const client = new Dropbox({ accessToken: process.env.DBX_ACCESS_TOKEN });
+  const client = new Dropbox({
+    refreshToken: process.env.DBX_REFRESH_TOKEN,
+    clientId: process.env.DBX_CLIENT_ID,
+    clientSecret: process.env.DBX_CLIENT_SECRET,
+  });
   const path = process.env.DBX_TARGET_PATH || "";
   const adapter = new DbxAdapter(client, path);
 
