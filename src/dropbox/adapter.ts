6785690c5b60f2628f9f12dcce78d331a9778f20
danimal141 <hideaki.ishii1204@gmail.com>
2023-10-27 16:43:57 +0900

    Revert "Update how to return links because of dropbox specification changes"
    This reverts commit 503b25d2b72b1f099dc0e7e142b8830bba3c7ba0.

--- src/dropbox/adapter.ts
+++ src/dropbox/adapter.ts
@@ -6,6 +6,7 @@ export default class DbxAdapter {
   static readonly TARGET_FILE_LIMIT = 5;
   static readonly ASSET_FILE_LIMIT = 100;
   static readonly REVIVE_FILE_LIMIT = 100;
+  static readonly DL_DROPBOX_URL_BASE = "https://dl.dropboxusercontent.com";
 
   private client: Dropbox;
   private rootPath: string;
@@ -31,7 +32,12 @@ export default class DbxAdapter {
         return created.result.url;
       })
     );
-    return links;
+    return compact(links).map((link) => {
+      const path = new URL(link).pathname;
+      // convert the direct link
+      // "www.dropbox.com" -> "dl.dropboxusercontent.com" / strip "?dl=0"
+      return DbxAdapter.DL_DROPBOX_URL_BASE + path;
+    });
   }
 
   public async evacuateRelearnedFiles(paths: string[]): Promise<number> {
