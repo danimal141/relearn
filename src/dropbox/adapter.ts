d6275d48469412cc13d107dd61111b64358cc440
danimal141 <hideaki.ishii1204@gmail.com>
2024-02-26 07:57:23 +0900

    Fix because dropbox specification looked to change

--- src/dropbox/adapter.ts
+++ src/dropbox/adapter.ts
@@ -6,7 +6,7 @@ export default class DbxAdapter {
   static readonly TARGET_FILE_LIMIT = 5;
   static readonly ASSET_FILE_LIMIT = 100;
   static readonly REVIVE_FILE_LIMIT = 100;
-  static readonly DL_DROPBOX_URL_BASE = "https://dl.dropboxusercontent.com";
+  // static readonly DL_DROPBOX_URL_BASE = "https://dl.dropboxusercontent.com";
 
   private client: Dropbox;
   private rootPath: string;
@@ -32,12 +32,13 @@ export default class DbxAdapter {
         return created.result.url;
       })
     );
-    return compact(links).map((link) => {
-      const path = new URL(link).pathname;
-      // convert the direct link
-      // "www.dropbox.com" -> "dl.dropboxusercontent.com" / strip "?dl=0"
-      return DbxAdapter.DL_DROPBOX_URL_BASE + path;
-    });
+    return links;
+    // return compact(links).map((link) => {
+    //   const path = new URL(link).pathname;
+    //   // convert the direct link
+    //   // "www.dropbox.com" -> "dl.dropboxusercontent.com" / strip "?dl=0"
+    //   return DbxAdapter.DL_DROPBOX_URL_BASE + path;
+    // });
   }
 
   public async evacuateRelearnedFiles(paths: string[]): Promise<number> {
